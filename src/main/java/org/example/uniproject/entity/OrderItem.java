b6e09402859f3d4421f4e7271090528bee1f09e5
L4NCET-dev <vadym.hlotka@gmail.com>
2026-03-06 22:32:06 +0200

    ended create entities

--- src/main/java/org/example/uniproject/entity/OrderItem.java
+++ src/main/java/org/example/uniproject/entity/OrderItem.java
@@ -14,7 +14,7 @@ import java.math.BigDecimal;
 @Builder
 @Entity
 @Table(name = "order_items")
-public class OrderItems {
+public class OrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
